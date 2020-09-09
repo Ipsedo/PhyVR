bab96d533c88baef846011adaf00d502930d9826
Ipsedo <berriens@gmail.com>
2020-09-09 21:14:01 +0200

    ready for MVC !

--- app/src/main/cpp/view/drawable.h
+++ app/src/main/cpp/view/drawable.h
@@ -5,4 +5,13 @@
 #ifndef PHYVR_NDK_DRAWABLE_H
 #define PHYVR_NDK_DRAWABLE_H
 
+struct gl_infos {
+
+};
+
+class Drawable {
+public:
+    virtual void draw(gl_infos) = 0;
+};
+
 #endif //PHYVR_NDK_DRAWABLE_H
